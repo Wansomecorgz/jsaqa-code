98a7fd410ffc02485965933a8c5be9100f4033ff
Pavel Efimov <gform7@gmail.com>
2022-10-09 19:26:51 +0300

    Task 1

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,9 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+  it("Same books names should not be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"])
+    ).toEqual(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"]);
+  });
 });
