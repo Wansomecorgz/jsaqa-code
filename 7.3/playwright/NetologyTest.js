48bbc46f042b072a37e7f299da57a8038ef31dea
Pavel Efimov <gform7@gmail.com>
2022-10-20 22:26:26 +0300

    Task 2 uncorrect realization

--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -1,16 +1,21 @@
-const { chromium } = require("playwright");
+import { chromium } from "playwright";
+import { email, password } from "./user";
+import { test, expect } from "@playwright/test";
 
 (async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
-    devtools: true
+    devtools: false,
   });
-  const page = await browser.newPage();
-  await page.goto("https://netology.ru");
-  await page.click("text=Каталог курсов");
-  await page.pause();
-
-  //assertion
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', email);
+  await page.fill('[placeholder="Пароль"]', password);
+  await page.click("text=Войти");
+  await expect(page.locator("text=Мои курсы и профессии")).toBeVisible();
+  // ---------------------
+  await context.close();
   await browser.close();
 })();
--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,10 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
+import { email, password } from "../user";
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
-
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
-
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
-
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+test("Should login with valid auth data test", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', email);
+  await page.fill('[placeholder="Пароль"]', password);
+  await page.click("text=Войти");
+  await expect(page.locator("text=Мои курсы и профессии")).toBeVisible();
 });
